cc611389fdee0392b2d6cacffc2403c4003b5cc8
wwb6673 <billybriggs33@gmail.com>
2019-03-04 19:19:13 -0600

    add build

---
+++ precache-manifest.60257b20d286c9b5a3f8c78fe2cd3a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77c63b7c75532c29b89d",
+    "url": "/static/css/main.582a5080.chunk.css"
+  },
+  {
+    "revision": "77c63b7c75532c29b89d",
+    "url": "/static/js/main.77c63b7c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "29cc3befbe8bcdb430de",
+    "url": "/static/css/2.65d5d98f.chunk.css"
+  },
+  {
+    "revision": "29cc3befbe8bcdb430de",
+    "url": "/static/js/2.29cc3bef.chunk.js"
+  },
+  {
+    "revision": "811bb7a10e87aaf789811eff680a0db7",
+    "url": "/index.html"
+  }
+];
